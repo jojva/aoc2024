f82335f7101c74acecdd875427f57991673e8197
Joris Valette <joris.valette@algolia.com>
2024-12-05 18:49:48 +0100

    Solution 2

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,10 +1,12 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
 
 fn main() {
     solution_1("data/input_1.txt");
+    solution_2("data/input_2.txt");
 }
 
 fn solution_1(filename: &str) {
@@ -31,4 +33,30 @@ fn solution_1(filename: &str) {
         total_diff += left.abs_diff(*right);
     }
     println!("{}", total_diff);
+}
+
+fn solution_2(filename: &str) {
+    let file = File::open(filename).expect("File not found");
+    let reader = BufReader::new(file);
+
+    let mut numbers = Vec::new();
+    let mut multipliers: HashMap<u32, u32> = HashMap::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let pairs: Vec<u32> = line
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect();
+        assert_eq!(pairs.len(), 2);
+        numbers.push(pairs[0]);
+        *multipliers.entry(pairs[1]).or_insert(0) += 1;
+    }
+
+    let mut occurrences: u32 = 0;
+    for number in numbers.iter() {
+        if let Some(multiplier) = multipliers.get(number) {
+            occurrences += number * multiplier;
+        }
+    }
+    println!("{}", occurrences);
 }
